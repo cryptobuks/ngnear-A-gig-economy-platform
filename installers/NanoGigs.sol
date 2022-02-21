f3fa8aa662e2dd0c5fbb48f392e71c9ef912e6d5
mwhc00 <melvinwonghc@gmail.com>
2022-02-21 12:02:46 +0900

    added installer files

---
+++ installers/NanoGigs.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract NanoGigs is Ownable, ERC20 {
+    constructor() public ERC20("GIGS", "GIG") { }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
